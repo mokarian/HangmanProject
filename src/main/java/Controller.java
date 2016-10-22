b8d00a7a21f1ffbdda0f2ae12792eec5445ad800
KATY <KATY@192.168.1.6>
2016-10-22 13:49:38 -0700

    New Player() changed to  new Guesser() and new Chooser()

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -8,8 +8,8 @@ public class Controller {
 
     public static void main(final String[] args) {
         System.out.print("Start");
-        final Guesser guesser = (Guesser) new Player();
-        final Chooser chooser = (Chooser) new Player();
+        final Guesser guesser = new Guesser();
+        final Chooser chooser = new Chooser();
         chooser.choose();
         final GameDifficulty difficulty = guesser.getDifficulty();
         final Game game = new Game(difficulty, chooser);
